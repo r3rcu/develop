79c66a53b6122974a8ded454b55cc97ee593d6dd
Miguel Sancho <sanchojaf@gmail.com>
2020-01-18 10:05:17 -0800

    Update reference to Odoo 13

--- omna/__manifest__.py
+++ omna/__manifest__.py
@@ -28,7 +28,7 @@
     'website': "https://www.omna.io/",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Extra Tools',
     'version': '0.1',
